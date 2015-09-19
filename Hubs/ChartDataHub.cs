5ed43323a60f5fa6f1c2d3efcdd8d3fa011ab0d8
Ivan Ivanov <slayer_word_11@outlook.com>
2015-09-19 09:03:22 +0300

    update solution

---
+++ Hubs/ChartDataHub.cs
@@ -0,0 +1,26 @@
+﻿namespace RealTimeChart.Hubs
+{
+    using Microsoft.AspNet.SignalR;
+    using Microsoft.AspNet.SignalR.Hubs;
+    using System.Collections.Generic;
+
+    [HubName("chartData")]
+    public class ChartDataHub : Hub
+    {
+        private readonly ChartData _pointer;
+
+        public ChartDataHub() : this(ChartData.Instance)
+        {
+        }
+
+        public ChartDataHub(ChartData pointer)
+        {
+            _pointer = pointer;
+        }
+
+        public IEnumerable<int> initData()
+        {
+            return _pointer.initData();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RealTimeChart.Startup))]
+namespace RealTimeChart
+{
+    public class Startup
+    {
+        private IAppBuilder app;
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
